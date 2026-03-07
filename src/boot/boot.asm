dd6a0481d2af911e9930a09245226b4c3e0c2184
Suyash Raj <suyash10581108@gmail.com>
2026-03-07 01:55:12 -0800

    First hello world bootloader working

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -22,12 +22,13 @@ print:
     ret             ; Return from the print function
 
 print_char:
-    mov ah, oeh     ; Move oeh into AH, this is the BIOS service number for printing a character
+    mov ah, 0eh     ; Move 0eh = 0x0E into AH, this is the BIOS service number for printing a character
     int 0x10        ; Call BIOS interrup 0x10, which handles screen operations
     ret             ; Returns from print_char function
 
 message: db 'Hello, World!', 0  ; Our null-terminated string to be printed
 
+; A boot sector is exactly 512 bytes = 510 + last 2 bytes
 times 510-($ - $$) db 0         ; Fill the rest of the sector with zeros upto 510 bytes
 
 dw 0xAA55           ; The boot sector signature
